1791d198ce88ae2ba1e53a82c2ad0253e1b3f706
origin10 <w.crows@gmail.com>
2017-02-20 20:49:43 +0800

    20170220 Night

---
+++ HwExp/Script/Lab01.js
@@ -0,0 +1,12 @@
+﻿var table = "<table><caption><h4>九九乘法表</h4></caption><tbody><tr>";
+for (var i = 2 ; i < 10 ; i++) {
+    table += "<td>";
+    for (var j = 1; j < 10 ; j++) {
+        v = i + "*" + j + "=" + i * j + "<br>";
+        table += v;
+    }
+    table += "</td>"
+}
+table += "</tr></tbody></table>"
+
+document.write(table);
